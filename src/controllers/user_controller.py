9a567ece9a6bdbd76763e10f8779d8fab88dae01
Logan Harris <loganrodharris@gmail.com>
2020-12-12 13:14:07 +1100

    added login to controllers.user_controller.user_register & added hashing to password

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -7,7 +7,23 @@ user = Blueprint("user", __name__, url_prefix="/userprofile")
 
 @user.route("/register", methods=["POST"])
 def user_register():
-    pass
+    user_fields = user_schema.load(request.json)
+
+    user = User.query.filter_by(email=user_fields["email"]).first()
+
+    if user:
+        return abort(400, description="Email already registered to an account")
+
+    user = User()
+    user.displayname = user_fields["displayname"]
+    user.username = user_fields["username"]
+    user.email = user_fields["email"]
+    user.password = bcrypt.generate_password_hash(user_fields["password"]).decode("utf-8")
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(user))
 
 @user.route("/login", methods=["GET"])
 def user_login():
