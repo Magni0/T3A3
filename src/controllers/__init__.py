4436130370282e1996a9340739b504876c925147
Logan Harris <loganrodharris@gmail.com>
2020-12-12 11:19:08 +1100

    added base config for flask app

---
+++ src/controllers/__init__.py
@@ -0,0 +1,5 @@
+
+
+registable_controllers = [
+    
+]
---
+++ src/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+ma = Marshmallow()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("default_settings.app_config")
+
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app)
+
+    from controllers import registable_controllers
+    for controller in registable_controllers:
+        app.register_blueprint(controller)
+
+    
