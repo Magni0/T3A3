0069391f93a683a7f5a9afb916a0bfb66a6f65d1
Logan Harris <loganrodharris@gmail.com>
2020-12-12 12:56:01 +1100

    added user_register & user_login endpoints in user_controller

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,1 +1,14 @@
 from models.User import User
+from schemas.UserSchema import user_schema
+from main import db, bcrypt
+from flask import Blueprint, request, jsonify, abort
+
+user = Blueprint("user", __name__, url_prefix="/userprofile")
+
+@user.route("/register", methods=["POST"])
+def user_register():
+    pass
+
+@user.route("/login", methods=["GET"])
+def user_login():
+    pass
