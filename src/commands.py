62114a8038b2053f13b62015c233b26a1b106516
Logan Harris <loganrodharris@gmail.com>
2020-12-13 12:00:01 +1100

    added flask custom command seed-user

--- src/commands.py
+++ src/commands.py
@@ -16,6 +16,41 @@ def drop_tables():
     db.engine.execute("DROP TABLE IF EXISTS alembic_version;")
     print("Deleted Tables")
 
+@db_commands.cli.command("seed-user")
+def seed_tables_user():
+    from main import bcrypt
+    from models.User import User
+
+    faker = Faker()
+    users = []
+
+    for i in range(10):
+        user = User()
+        user.displayname = f"testuser{i}"
+        user.email = f"testemail{i}@test.com"
+        user.username = f"testusername{i}"
+        user.password = bcrypt.generate_password_hash("testpassword").decode("utf-8")
+        db.session.add(user)
+        users.append(user)
+    
+    db.session.commit()
+
+@db_commands.cli.command("seed-images")
+def seed_tables_images():
+    pass
+
+@db_commands.cli.command("seed-moods")
+def seed_tables_moods():
+    pass
+
+@db_commands.cli.command("seed-tracks")
+def seed_tables_tracks():
+    pass
+
+@db_commands.cli.command("seed-artist")
+def seed_tables_artist():
+    pass
+
 @db_commands.cli.command("seed")
 def seed_tables():
     pass
